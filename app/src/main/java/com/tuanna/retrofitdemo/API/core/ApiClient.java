1ca253aed66497fd674c5c3a742661a1144fe2a4
Nguyen Anh Tuan <tuanna@asiantech.vn>
2015-11-02 16:19:13 +0700

    update ApiClient

--- app/src/main/java/com/tuanna/retrofitdemo/API/core/ApiClient.java
+++ app/src/main/java/com/tuanna/retrofitdemo/API/core/ApiClient.java
@@ -11,6 +11,8 @@ import com.squareup.okhttp.Response;
 import com.tuanna.retrofitdemo.API.Api;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
@@ -24,13 +26,9 @@ public class ApiClient {
 
     private static final String HEADER = "User-Agent";
 
-    private static ApiClient sApiClient;
+    private static final int TIMEOUT_CONNECTION = 10000;
 
-    private Retrofit mRetrofit;
-    /**
-     * OkHttpClient:by OkHttp
-     */
-    private OkHttpClient mOkHttpClient;
+    private static ApiClient sApiClient;
     /**
      * Api service
      */
@@ -39,12 +37,25 @@ public class ApiClient {
      * android application context
      */
     private Context mContext;
+    private SessionStore mSessionStore;
 
     // Define the interceptor, add authentication headers
     private Interceptor mInterceptor = new Interceptor() {
         @Override
         public Response intercept(Chain chain) throws IOException {
-            Request newRequest = chain.request().newBuilder().addHeader(HEADER, createUserAgent()).build();
+            Request newRequest = null;
+
+            Map<String, String> headers = mSessionStore.header(mContext);
+
+            if (headers.size() > 0) {
+                for (Map.Entry<String, String> entry : headers.entrySet()) {
+                    newRequest = chain.request().newBuilder()
+                            .addHeader(entry.getKey(), entry.getValue()).build();
+                }
+            } else {
+                newRequest = chain.request().newBuilder().addHeader(HEADER, createUserAgent()).build();
+            }
+
             return chain.proceed(newRequest);
         }
     };
@@ -52,7 +63,7 @@ public class ApiClient {
     /**
      * get singleton instance
      *
-     * @return
+     * @return singleton instance
      */
     public static synchronized ApiClient getInstance() {
         if (sApiClient == null) {
@@ -65,13 +76,17 @@ public class ApiClient {
         return getInstance().mApi;
     }
 
-    public void init(String baseUrl) {
+    public void init(ApiConfig apiConfig) {
+        mContext = apiConfig.mContext;
+        mSessionStore = apiConfig.sessionStore;
+
         // Add the interceptor to OkHttpClient
-        mOkHttpClient = new OkHttpClient();
+        OkHttpClient mOkHttpClient = new OkHttpClient();
+        mOkHttpClient.setConnectTimeout(TIMEOUT_CONNECTION, TimeUnit.MILLISECONDS);
         mOkHttpClient.interceptors().add(mInterceptor);
 
-        mRetrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
+        Retrofit mRetrofit = new Retrofit.Builder()
+                .baseUrl(apiConfig.baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(mOkHttpClient)
                 .build();
